846b241e0c612081b06a28351e1bcae19bb8fea3
Raymond Wai <raymondwai@asl.com.hk>
2023-05-16 11:42:40 +0800

    Modified UI v4

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Women 🐺")
-option_b = os.getenv('OPTION_B', u"Men 🐶")
+option_a = os.getenv('OPTION_A', u"Cats 🐺")
+option_b = os.getenv('OPTION_B', u"Dogs 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
